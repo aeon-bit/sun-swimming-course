0102264f40c7c80bbff397de1ccdd3bd83c841f2
unknown <ionegatsuga@gmail.com>
2022-07-19 20:46:49 +0700

    19 juli 5

--- app/src/main/java/com/yasinta/kesehatankucing/ui/chat/ChatFragment.java
+++ app/src/main/java/com/yasinta/kesehatankucing/ui/chat/ChatFragment.java
@@ -1,11 +1,14 @@
 package com.yasinta.kesehatankucing.ui.chat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.yasinta.kesehatankucing.R;
@@ -16,6 +19,17 @@ public class ChatFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        CardView cv_btnPerformChatDokter = root.findViewById(R.id.cv_btnPerformChatDokter);
+        cv_btnPerformChatDokter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://api.whatsapp.com/send?phone=" + "08156754523";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
         return root;
     }
 
