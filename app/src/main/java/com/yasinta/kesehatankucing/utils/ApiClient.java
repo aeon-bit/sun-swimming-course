57fc5dcb494787bb45d89039a40b415d5859dc23
unknown <ionegatsuga@gmail.com>
2022-08-01 15:21:38 +0700

    1 aug 1

--- app/src/main/java/com/yasinta/kesehatankucing/utils/ApiClient.java
+++ app/src/main/java/com/yasinta/kesehatankucing/utils/ApiClient.java
@@ -5,11 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://192.168.8.102:8000/";
+//    public static final String BASE_URL = "http://192.168.8.102:8000/";
 //    public static final String BASE_URL = "http://192.168.0.200:8000/";
 //    public static final String BASE_URL = "http://192.168.43.206:8000/";
 //    public static final String BASE_URL = "http://192.168.57.101:8000/";
-//    public static final String BASE_URL = "http://127.0.0.1:8000/";
+    public static final String BASE_URL = "https://admin-page-diagnosis-penyakit-kucing.my.id/";
 
     public static final String IMAGE_URL = BASE_URL + "storage/images/";
 
